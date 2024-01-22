fdd79184fbe55c7cb4c9f291ceb1a17bdda5f257
Graham Williams <graham.williams@togaware.com>
2024-01-22 20:42:14 +1100

    Add timer, sound gong, keep screen awake.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
-import 'package:solid/solid.dart';
+// import 'package:solid/solid.dart';
+
+import 'package:innerpod/timer.dart';
 
 void main() {
   runApp(const InnerPod());
@@ -13,23 +15,25 @@ class InnerPod extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Key POD',
-      theme: ThemeData(
-        // This is the theme of your application.
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      title: 'Inner Pod',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Inner Pod Meditation Timer'),
+        ),
+        body: Center(
+          child: Timer(),
+        ),
       ),
-      home: const SolidLogin(
-        // Images generated using Bing Image Creator from Designer, powered by
-        // DALL-E3.
+      // home: const SolidLogin(
+      //   // Images generated using Bing Image Creator from Designer, powered by
+      //   // DALL-E3.
 
-        image: AssetImage('assets/images/inner_image.jpg'),
-        logo: AssetImage('assets/icon/icon.png'),
-        title: 'MANAGE YOUR INNER POD',
-        link: 'https://github.com/gjwgit/inner',
-        child: Scaffold(body: Text('Inner Pod Placeholder')),
-      ),
+      //   image: AssetImage('assets/images/inner_image.jpg'),
+      //   logo: AssetImage('assets/icon/icon.png'),
+      //   title: 'MANAGE YOUR INNER POD',
+      //   link: 'https://github.com/gjwgit/inner',
+      //   child: Scaffold(body: Text('Inner Pod Placeholder')),
+      // ),
     );
   }
 }
--- linux/flutter/generated_plugins.cmake
+++ linux/flutter/generated_plugins.cmake
@@ -3,6 +3,7 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  audioplayers_linux
   fast_rsa
   url_launcher_linux
 )
