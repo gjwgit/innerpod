553174b8141d6470c824f1fe9f50e77620e4ab3f
Graham Williams <graham.williams@togaware.com>
2024-01-24 09:52:29 +1100

    Add Start and Stop buttons.

--- lib/timer.dart
+++ lib/timer.dart
@@ -1,6 +1,6 @@
-/// A countdown timer for a meditation session.
+/// A countdown timer and Start button for a session.
 //
-// Time-stamp: <Tuesday 2024-01-23 11:14:47 +1100 Graham Williams>
+// Time-stamp: <Wednesday 2024-01-24 09:49:40 +1100 Graham Williams>
 //
 /// Copyright (C) 2024, Togaware Pty Ltd
 ///
@@ -37,47 +37,100 @@ class Timer extends StatelessWidget {
   final controller = CountDownController();
   final player = AudioPlayer();
 
-  // Break this out into its own function because of the need for it to be
-  // async through the await.
+  // Add 1 to the duration to see 20:00 momentarily rather than 19:59, for
+  // example.
+
+  final duration = (20 * 60) + 1;
+
+  // Set the style for the text of the buttons.
+
+  final buttonTextStyle = const TextStyle(fontSize: 30);
+
+  // A small spacer for the buttons.
+
+  final spacer = const SizedBox(width: 20);
+
+  // The sound to be played at the beginning and end of a session, being a
+  // [Source] from audioplayers.
+
+  final dong = AssetSource('sounds/dong.ogg');
+
+  // We encapsulate the playing of the dong into its own function because of the
+  // need for it to be async through the await.
 
   Future<void> _dingDong() async {
-    await player.play(AssetSource('sounds/dong.ogg'));
+    await player.play(dong);
   }
 
   @override
   Widget build(BuildContext context) {
-    return NeonCircularTimer(
-      width: 250,
-      duration: 20 * 60,
-      controller: controller,
-      backgroudColor: Colors.blueGrey.shade600,
-      textStyle: const TextStyle(
-        color: Colors.white,
-        fontSize: 55,
-      ),
-      onStart: () {
-        WakelockPlus.enable();
-        _dingDong();
-      },
-      onComplete: () {
-        _dingDong();
-        WakelockPlus.disable();
-      },
-      isReverse: true,
-      isReverseAnimation: true,
-      innerFillGradient: LinearGradient(
-        colors: [
-          Colors.blueAccent.shade100,
-          Colors.blueAccent.shade700,
+    return Padding(
+      padding: const EdgeInsets.only(top: 50),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          NeonCircularTimer(
+            width: 250,
+            duration: duration,
+            controller: controller,
+            autoStart: false,
+            backgroudColor: Colors.blueGrey.shade600,
+            textStyle: const TextStyle(
+              color: Colors.white,
+              fontSize: 55,
+            ),
+            onComplete: () {
+              _dingDong();
+              WakelockPlus.disable();
+            },
+            isReverse: true,
+            isReverseAnimation: true,
+            innerFillGradient: LinearGradient(
+              colors: [
+                Colors.blueAccent.shade100,
+                Colors.blueAccent.shade700,
+              ],
+            ),
+            neonGradient: LinearGradient(
+              colors: [
+                Colors.blueAccent.shade100,
+                Colors.blueAccent.shade700,
+              ],
+            ),
+            // initialDuration: 5, // THIS IS THE TIME ALREADY COMPLETED
+          ),
+          Padding(
+            padding: const EdgeInsets.all(60),
+            child: Row(
+              children: [
+                spacer,
+                ElevatedButton(
+                  style: TextButton.styleFrom(
+                    textStyle: buttonTextStyle,
+                  ),
+                  onPressed: () {
+                    _dingDong();
+                    controller.restart();
+                    WakelockPlus.enable();
+                  },
+                  child: const Text('Start'),
+                ),
+                spacer,
+                ElevatedButton(
+                  style: TextButton.styleFrom(
+                    textStyle: buttonTextStyle,
+                  ),
+                  onPressed: () {
+                    controller.pause();
+                    WakelockPlus.disable();
+                  },
+                  child: const Text('Stop'),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
-      neonGradient: LinearGradient(
-        colors: [
-          Colors.blueAccent.shade100,
-          Colors.blueAccent.shade700,
-        ],
-      ),
-      // initialDuration: 5, // THIS IS THE TIME ALREADY COMPLETED
     );
   }
 }
