d3da99fbec5d1a536840d0d268eb6d7f670bd19d
Graham Williams <graham.williams@togaware.com>
2024-02-04 11:25:57 +1100

    Cleanup main.dart and prep for merge into main branch.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 /// Main program for the inner pod session timing and logging.
 //
-// Time-stamp: <Sunday 2024-02-04 11:18:02 +1100 Graham Williams>
+// Time-stamp: <Sunday 2024-02-04 11:25:34 +1100 Graham Williams>
 //
 /// Copyright (C) 2024, Togaware Pty Ltd
 ///
@@ -38,101 +38,12 @@ void main() {
   // Call MaterialApp() here rather than within InnerPod so that
   // MaterialLocalizations is found when firing off the showAboutDialog.
 
-  runApp(MaterialApp(
-      title: 'Inner Pod',
-      debugShowCheckedModeBanner: false,
-      //     theme: ThemeData(
-      //         // Change the theme for the app here.
-//        cardTheme: const CardTheme(
-//          color: border,
-//        ),
-//          ),
-      home: const InnerPod()));
-}
-
-class _InnerPodScaffold extends StatefulWidget {
-  // Construct the Scaffold as the main window to display after logging in to
-  // the Solid Pod. This is split out as a separate class and is Stateful to
-  // support the state of the navbar.
-
-  const _InnerPodScaffold();
-
-  @override
-  _InnerPodScaffoldState createState() => _InnerPodScaffoldState();
-}
-
-class _InnerPodScaffoldState extends State<_InnerPodScaffold> {
-  // Construct the Scaffold as the main window to display after logging in to
-  // the Solid Pod.
-
-  // This widget is the root of the application.
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: background,
-      appBar: AppBar(
-        title: const Text('Inner Pod Session Timer'),
-        backgroundColor: border,
-//        foregroundColor: Colors.black,
-        actions: [
-          // RUN
-
-          IconButton(
-            key: const Key('run_button'),
-            icon: const Icon(
-              //Icons.directions_run,
-              Icons.info,
-              color: Colors.grey,
-            ),
-            onPressed: () {
-              showAboutDialog(
-                context: context,
-                applicationName: 'Inner Pod',
-                applicationVersion: '0.0.0',
-                applicationIcon:
-                    const ImageIcon(AssetImage('assets/images/inner_icon.png')),
-                children: [
-                  const SelectableText('A session timer with logging.\n\n'
-                      'Inner Pod is an app for timing sessions and storing'
-                      ' sessions to your Pod. A session can be anything though'
-                      ' the app is commonly used for contemplative'
-                      ' meditation.\n\n'
-                      'The app is written in Flutter and the open source code'
-                      ' is available from github at'
-                      ' https://github.com/gjwgit/innerpod.'
-                      ' You can try it out online at'
-                      ' https://innerpod.solidcommunity.au.\n\n'
-                      ' This app is written by Graham Williams.'),
-                ],
-              );
-            },
-            tooltip: 'Popup a window about the app.',
-          ),
-        ],
-      ),
-      body: Center(
-        child: Timer(),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: border,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home, color: Colors.black),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.text_snippet),
-            label: 'Text',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'History',
-          ),
-        ],
-      ),
-    );
-  }
+  runApp(
+    const MaterialApp(
+        title: 'Inner Pod',
+        debugShowCheckedModeBanner: false,
+        home: InnerPod()),
+  );
 }
 
 /// The primary widget for the app.
@@ -151,7 +62,7 @@ class InnerPod extends StatelessWidget {
     // View History functionality that will then prompt to login to the POD at
     // that time.
 
-    // Not using Solid Login for now.
+    // TODO 20240204 gjw NOT USING SolidLogin() FOR NOW.
 
     // return const SolidLogin(
     //   title: 'MANAGE YOUR INNER POD',
@@ -163,6 +74,59 @@ class InnerPod extends StatelessWidget {
     //   link: 'https://github.com/gjwgit/innerpod/blob/main/README.md',
     //   child: _InnerPodScaffold(),
     // );
-    return const _InnerPodScaffold();
+
+    return Scaffold(
+      backgroundColor: background,
+      appBar: AppBar(
+        title: const Text('Inner Pod Session Timer'),
+        backgroundColor: border,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.info),
+            onPressed: () {
+              showAboutDialog(
+                context: context,
+                applicationName: 'Inner Pod',
+                applicationVersion: '0.0.0',
+                applicationIcon:
+                    const ImageIcon(AssetImage('assets/images/inner_icon.png')),
+                children: [
+                  const SelectableText('A session timer with logging.\n\n'
+                      'Inner Pod is an app for timing sessions and storing'
+                      ' sessions to your Pod. A session can be anything though'
+                      ' the app is commonly used for contemplative'
+                      ' meditation.\n\n'
+                      'The app is written in Flutter and the open source code'
+                      ' is available from github at'
+                      ' https://github.com/gjwgit/innerpod.'
+                      ' You can try it out online at'
+                      ' https://innerpod.solidcommunity.au.\n\n'
+                      ' Authors: Graham Williams.'),
+                ],
+              );
+            },
+            tooltip: 'Popup a window about the app.',
+          ),
+        ],
+      ),
+      body: Center(child: Timer()),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: border,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home, color: Colors.black),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.text_snippet),
+            label: 'Text',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'History',
+          ),
+        ],
+      ),
+    );
   }
 }
