b91e80d0f6424df5284bd6303544d2f4ce320b89
Graham Williams <graham.williams@togaware.com>
2025-10-06 09:24:35 +1100

    Release to google play store

--- support/flutter.mk
+++ support/flutter.mk
@@ -441,7 +441,8 @@ apk::
 	cp build/app/outputs/flutter-apk/app-release.apk installers/$(APP).apk
 	cp build/app/outputs/flutter-apk/app-release.apk installers/$(APP)-$(VER).apk
 
-appbundle:
+appbundle::
+	flutter clean
 	flutter build appbundle --release
 	cp build/app/outputs/bundle/release/app-release.aab installers/$(APP).aab
 	cp build/app/outputs/bundle/release/app-release.aab installers/$(APP)-$(VER).aab
