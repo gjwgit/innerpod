8ac430b54b92de73351fa51084cb4a6708ebd6b6
Amogh Hosamane <amoghvarsh9614@gmail.com>
2026-02-13 15:53:55 +0530

    Fix import ordering in home.dart and main.dart #16 #17

--- lib/home.dart
+++ lib/home.dart
@@ -26,9 +26,8 @@ library;
 
 import 'package:flutter/material.dart';
 
-import 'package:solidui/solidui.dart';
-
 import 'package:package_info_plus/package_info_plus.dart';
+import 'package:solidui/solidui.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import 'package:innerpod/constants/colours.dart';
