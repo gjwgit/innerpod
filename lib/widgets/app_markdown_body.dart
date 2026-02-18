7cbefeff2d01afdf8bca7d824abe8772dd07fb7b
Amoghhosamane <amoghvarsh9614@gmail.com>
2026-02-19 00:56:21 +0530

    Correct import grouping in app_markdown_body.dart

--- lib/widgets/app_markdown_body.dart
+++ lib/widgets/app_markdown_body.dart
@@ -15,7 +15,6 @@ library;
 import 'package:flutter/material.dart';
 
 import 'package:flutter_markdown_plus/flutter_markdown_plus.dart';
-
 import 'package:url_launcher/url_launcher.dart';
 
 /// A reusable markdown body widget with selectable text and clickable links.
