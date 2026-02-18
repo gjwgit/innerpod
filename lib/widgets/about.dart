95b2adf74e021f30797ffd338862cb941f4186d4
Amoghhosamane <amoghvarsh9614@gmail.com>
2026-02-19 00:59:13 +0530

    Fix import ordering and copyright headers to satisfy CI lints

--- lib/widgets/about.dart
+++ lib/widgets/about.dart
@@ -30,8 +30,8 @@ import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:solidpod/solidpod.dart';
 
-import 'package:innerpod/utils/word_wrap.dart';
 import 'package:innerpod/widgets/app_markdown_body.dart';
+import 'package:innerpod/utils/word_wrap.dart';
 
 /// Display the about dialog for the app.
 
