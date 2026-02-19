a156bc560c19eed070d57a9891c9c4f1b7464174
Amoghhosamane <amoghvarsh9614@gmail.com>
2026-02-19 09:53:26 +0530

    Add license header and documentation to app_markdown_body

--- lib/widgets/app_markdown_body.dart
+++ lib/widgets/app_markdown_body.dart
@@ -1,27 +1,11 @@
-/// A reusable markdown body widget with selectable text and clickable links.
-//
-// Time-stamp: <2025-02-18 20:40:00 Graham Williams>
-//
-/// Copyright (C) 2024-2026, Togaware Pty Ltd
+/*
+ * Copyright (c) 2024–2026 Graham Williams
+ * Licensed under the MIT License.
+ */
+
+/// A reusable widget for rendering markdown content.
 ///
-/// Licensed under the GNU General Public License, Version 3 (the "License");
-///
-/// License: https://opensource.org/license/gpl-3-0.
-//
-// This program is free software: you can redistribute it and/or modify it under
-// the terms of the GNU General Public License as published by the Free Software
-// Foundation, either version 3 of the License, or (at your option) any later
-// version.
-//
-// This program is distributed in the hope that it will be useful, but WITHOUT
-// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-// details.
-//
-// You should have received a copy of the GNU General Public License along with
-// this program.  If not, see <https://opensource.org/license/gpl-3-0>.
-///
-/// Authors: Graham Williams
+/// Provides consistent markdown styling across the app.
 
 library;
 
